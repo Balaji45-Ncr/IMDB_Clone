f9aa358510be5933a18888ff1cf995701dbeeb2e
Balaji45-Ncr <ncrlpb45@gmail.com>
2025-05-20 14:52:00 +0530

    'validation'

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -17,23 +17,21 @@ class MovieSerializer(serializers.Serializer):
         instance.active=validated_data.get('active',instance.active)
         instance.save()
         return instance
-    # def validate_name(self, value):
-    #     if len(value)<5 or len(value)>50:
-    #         raise serializers.ValidationError('Name should be between 5 and 50 characters long.')
-    #     return value
     
-    # def validate_description(self, value):  
-    #     if len(value)<10 or len(value)>1000:
-    #         raise serializers.ValidationError('Description should be between 10 and 1000 characters long.')
-    #     return value
-    # def validate_active(self, value):
-    #     if value not in [True,False]:
-    #         raise serializers.ValidationError('Active should be either True or False.')
-    #     return value
-    # def validate_id(self, value):
-    #     movie=Movie.objects.filter(id=value).first()
-    #     if not movie:
-    #         raise serializers.ValidationError('Movie with given id does not exist.')
-    #     return value
-
-       
+    def validate_name(self,value):
+        if len(value)<5 :
+            raise serializers.ValidationError('Name should be at least 5 characters long')  
+        return value
+    def validate_description(self,value):
+        if len(value)<10 :
+            raise serializers.ValidationError('Description should be at least 10 characters long')  
+        return value
+    def validate(self, data):
+        if data['name'] ==data['description']:
+            raise serializers.ValidationError('Name and Description should not be same')    
+        return data
+    def validate_active(self, value):   
+        if value not in [True,False]:
+            raise serializers.ValidationError('Active should be either True or False')  
+        return value    
+   
