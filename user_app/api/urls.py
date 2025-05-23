87506a2dc968740e47399bda02a96957241ff47f
Balaji45-Ncr <ncrlpb45@gmail.com>
2025-05-24 01:55:36 +0530

    'jwt_config'

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -2,7 +2,14 @@ from django.contrib import auth
 from django.urls import path,re_path,include
 
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('login/',obtain_auth_token,name='login')
+    # path('login/',obtain_auth_token,name='login')
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
